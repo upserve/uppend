78fd84c680ad7399e2b96b09fe0bcaf785a45376
David Stuebe <davidstuebe@swipely.com>
2017-03-21 13:39:34 -0400

    Add scan and stream read to append store interface

--- src/main/java/com/upserve/uppend/AppendOnlyStore.java
+++ src/main/java/com/upserve/uppend/AppendOnlyStore.java
@@ -1,6 +1,7 @@
 package com.upserve.uppend;
 
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 /**
  * Defines the minimum interface required to add byte arrays under a key, and to
@@ -15,7 +16,7 @@ public interface AppendOnlyStore extends AutoCloseable {
      * @param value the value to append
      */
     void append(String key, byte[] value);
-
+    
     /**
      * Read byte arrays that have been stored under a given key
      *
@@ -23,6 +24,20 @@ public interface AppendOnlyStore extends AutoCloseable {
      * @param reader function to be called once per stored byte array
      */
     void read(String key, Consumer<byte[]> reader);
+
+    /**
+     * Read byte arrays that have been stored under a given key as a stream
+     *
+     * @param key the key under which to retrieve
+     */
+    Stream<byte[]> read(String key);
+
+    /**
+     * Stream of the keys in the append store instance
+     *
+     * @param prefix the key prefix for a set of stored objects. All keys if null.
+     */
+    Stream<String> scan(String prefix);
 
     /**
      * Remove all keys and values from the store.
