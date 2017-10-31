19cf9d901bc91a07a88b002e31e46c16a015adcb
Bright Fulton <bright.fulton@gmail.com>
2017-10-31 01:06:32 -0400

    add ability to get last value to append only store
    fix test and off by one error

--- src/main/java/com/upserve/uppend/AppendOnlyStore.java
+++ src/main/java/com/upserve/uppend/AppendOnlyStore.java
@@ -29,6 +29,16 @@ public interface AppendOnlyStore extends AutoCloseable {
     Stream<byte[]> read(String partition, String key);
 
     /**
+     * Read the last byte array that was stored under a given key
+     *
+     * @param partition the partition under which to retrieve
+     * @param key the key under which to retrieve
+     * @throws IllegalArgumentException if partition is invalid
+     * @return the stored byte array, or null if none
+     */
+    byte[] readLast(String partition, String key);
+
+    /**
      * Enumerate the keys in the data store
      *
      * @param partition the partition under which to retrieve
