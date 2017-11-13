57aaa15dd21bc3401bd047d92e1205dbf366e6b7
Bright Fulton <bright.fulton@gmail.com>
2017-11-13 09:30:58 -0500

    improve coverage
    * add test for BlockedLongs
    * add tests build builder interface default methods
    * removed unused FilterStream class

---
+++ src/test/java/com/upserve/uppend/CounterStoreBuilderTest.java
@@ -0,0 +1,22 @@
+package com.upserve.uppend;
+
+import com.codahale.metrics.MetricRegistry;
+import com.upserve.uppend.util.SafeDeleting;
+import org.junit.Test;
+
+import java.nio.file.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class CounterStoreBuilderTest {
+
+    @Test
+    public void testBuildWithMetrics() throws Exception {
+        Path path = Paths.get("build/tmp/test/counter-store-builder");
+        SafeDeleting.removeDirectory(path);
+        MetricRegistry metrics = new MetricRegistry();
+        CounterStore store = Uppend.counterStore(path).buildWithMetrics(metrics);
+        store.flush();
+        assertEquals(1, metrics.getTimers().get("flush").getCount());
+    }
+}
