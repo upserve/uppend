890058f7fb3453f8f2407a7ea59e699dedbed2fe
Bright Fulton <bright.fulton@gmail.com>
2017-12-26 18:53:45 -0500

    add blocked longs test coverage

--- src/test/java/com/upserve/uppend/BlockedLongsTest.java
+++ src/test/java/com/upserve/uppend/BlockedLongsTest.java
@@ -3,17 +3,19 @@ package com.upserve.uppend;
 import com.upserve.uppend.util.SafeDeleting;
 import org.junit.*;
 
+import java.io.UncheckedIOException;
 import java.nio.file.*;
 
 import static org.junit.Assert.*;
 
 public class BlockedLongsTest {
     private Path path = Paths.get("build/test/tmp/block");
+    private Path posPath = path.resolveSibling(path.getFileName() + ".pos");
 
     @Before
     public void initialize() throws Exception {
         SafeDeleting.removeTempPath(path);
-        SafeDeleting.removeTempPath(path.resolveSibling("block.pos"));
+        SafeDeleting.removeTempPath(posPath);
     }
 
     @Test
@@ -22,6 +24,15 @@ public class BlockedLongsTest {
         new BlockedLongs(path, 10);
         new BlockedLongs(path, 100);
         new BlockedLongs(path, 1000);
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void testCtorNoPosFile() throws Exception {
+        BlockedLongs block = new BlockedLongs(path, 1);
+        block.close();
+        Files.delete(posPath);
+        Files.createDirectories(posPath);
+        new BlockedLongs(path, 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -70,6 +81,17 @@ public class BlockedLongsTest {
         }, v.values(pos2).toArray());
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void testAppendAtNonStartingBlock() throws Exception {
+        BlockedLongs v = new BlockedLongs(path, 10);
+        long pos1 = v.allocate();
+        for (long i = 0; i < 21; i++) {
+            v.append(pos1, i);
+        }
+        int blockSize = 16 + 10 * 8; // mirrors BlockedLongs.blockSize
+        v.append(blockSize * 2, 21);
+    }
+
     @Test
     public void testLastValue() throws Exception {
         BlockedLongs v = new BlockedLongs(path, 4);
@@ -78,5 +100,14 @@ public class BlockedLongsTest {
             v.append(pos, i);
         }
         assertEquals(256, v.lastValue(pos));
+    }
+
+    @Test
+    public void testFlushAndCloseTwice() throws Exception {
+        BlockedLongs block = new BlockedLongs(path, 1);
+        block.flush();
+        block.flush();
+        block.close();
+        block.close();
     }
 }
