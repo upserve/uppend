0593331f55697b7712361645d83da2c3362949bd
Bright Fulton <bright.fulton@gmail.com>
2017-12-27 00:26:12 -0500

    add coverage for throwing blobs close

--- src/test/java/com/upserve/uppend/BlobsTest.java
+++ src/test/java/com/upserve/uppend/BlobsTest.java
@@ -3,7 +3,10 @@ package com.upserve.uppend;
 import com.upserve.uppend.util.SafeDeleting;
 import org.junit.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.*;
+import java.nio.*;
+import java.nio.channels.*;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
@@ -54,5 +57,105 @@ public class BlobsTest {
         blobs.close();
         blobs = new Blobs(Paths.get("build/test/blobs"));
         assertEquals("foo", new String(blobs.read(0)));
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void testCloseException() throws Exception {
+        resetFinal(blobs, "blobs", new FileChannel() {
+            @Override
+            public int read(ByteBuffer dst) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long read(ByteBuffer[] dsts, int offset, int length) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public int write(ByteBuffer src) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long write(ByteBuffer[] srcs, int offset, int length) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long position() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public FileChannel position(long newPosition) throws IOException {
+                return null;
+            }
+
+            @Override
+            public long size() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public FileChannel truncate(long size) throws IOException {
+                return null;
+            }
+
+            @Override
+            public void force(boolean metaData) throws IOException {
+
+            }
+
+            @Override
+            public long transferTo(long position, long count, WritableByteChannel target) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long transferFrom(ReadableByteChannel src, long position, long count) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public int read(ByteBuffer dst, long position) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public int write(ByteBuffer src, long position) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public MappedByteBuffer map(MapMode mode, long position, long size) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileLock lock(long position, long size, boolean shared) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileLock tryLock(long position, long size, boolean shared) throws IOException {
+                return null;
+            }
+
+            @Override
+            protected void implCloseChannel() throws IOException {
+                throw new IOException("expected");
+            }
+        });
+        blobs.close();
+    }
+
+    private static void resetFinal(Object inst, String fieldName, Object val) throws Exception {
+        Field field = inst.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(inst, val);
     }
 }
