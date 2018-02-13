9926d47045f06b516a8cd70b5cfa837a9b42fd63
David Stuebe <stu3b3@gmail.com>
2018-02-13 12:09:00 -0500

    Make flusher pool user threads

--- src/main/java/com/upserve/uppend/AutoFlusher.java
+++ src/main/java/com/upserve/uppend/AutoFlusher.java
@@ -23,7 +23,7 @@ public class AutoFlusher {
 
     static {
         ThreadGroup threadGroup = new ThreadGroup("auto-flush");
-        threadGroup.setDaemon(true);
+        threadGroup.setDaemon(false);
         AtomicInteger threadNumber = new AtomicInteger();
         threadFactory = r -> {
             Thread t = new Thread(threadGroup, r, "auto-flush-" + threadNumber.incrementAndGet());
