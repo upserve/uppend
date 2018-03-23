f16399f476d153f2eb011d893fc61f40c8045cf3
David Stuebe <stu3b3@gmail.com>
2018-03-23 15:56:35 -0400

    Use smaller hash size for test

--- src/test/java/com/upserve/uppend/LongLookupPerformanceTest.java
+++ src/test/java/com/upserve/uppend/LongLookupPerformanceTest.java
@@ -20,7 +20,7 @@ public class LongLookupPerformanceTest {
         SafeDeleting.removeTempPath(lookupDir);
 
         LongLookup lookup;
-        lookup = new LongLookup(lookupDir);
+        lookup = new LongLookup(lookupDir, 512, 512);
         long lastReportTime = System.currentTimeMillis();
         log.info("init: starting");
         for(int i = 0; i < INITIAL_KEYS; ++i) {
