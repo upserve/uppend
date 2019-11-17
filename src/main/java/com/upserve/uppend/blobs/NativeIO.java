5392a9b57eea8ea63ac8ddfd801db5d3ac488ddf
David Stuebe <stu3b3@gmail.com>
2019-11-17 17:37:25 -0500

    Attempt to load nativeIO library from jar

--- src/main/java/com/upserve/uppend/blobs/NativeIO.java
+++ src/main/java/com/upserve/uppend/blobs/NativeIO.java
@@ -1,9 +1,12 @@
 package com.upserve.uppend.blobs;
 
 import org.slf4j.Logger;
-import java.io.IOException;
+
+import java.io.*;
 import java.lang.invoke.MethodHandles;
+import java.net.URL;
 import java.nio.MappedByteBuffer;
+import java.nio.file.Files;
 
 public class NativeIO {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -19,7 +22,31 @@ public class NativeIO {
     public static native void madvise(MappedByteBuffer buffer, Advice advise) throws IOException;
 
     static {
-        log.info("loading nativeIO libbrary");
-        System.loadLibrary("nativeIO");
+        log.info("loading nativeIO libbrary...");
+
+        try{
+            System.loadLibrary("nativeIO");
+            log.info("System.loadLibrary(\"nativeIO\") - successful!");
+        } catch (UnsatisfiedLinkError e) {
+
+            String libName = "libnativeIO.dylib"; // The name of the file in resources/ dir
+            URL url = NativeIO.class.getResource("/" + libName);
+            File tmpDir = null;
+            try {
+                tmpDir = Files.createTempDirectory("nativeIO-lib").toFile();
+            } catch (IOException ex) {
+                throw new UncheckedIOException("Could not create tmpdir for nativeIO library", ex);
+            }
+            tmpDir.deleteOnExit();
+            File nativeLibTmpFile = new File(tmpDir, libName);
+            nativeLibTmpFile.deleteOnExit();
+            try (InputStream in = url.openStream()) {
+                Files.copy(in, nativeLibTmpFile.toPath());
+            } catch (IOException ex) {
+                throw new UncheckedIOException("Could not copy libnativeIO", ex);
+            }
+            System.load(nativeLibTmpFile.getAbsolutePath());
+            log.info("System.load(nativeLibTmpFile.getAbsolutePath()); - successful!");
+        }
     }
 }
