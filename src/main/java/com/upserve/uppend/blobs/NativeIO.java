894771999552f6ee38cbb7b895fe16997b1efffa
David Stuebe <stu3b3@gmail.com>
2019-11-18 20:06:20 -0500

    Use JNR-FFI to implement madvise

--- src/main/java/com/upserve/uppend/blobs/NativeIO.java
+++ src/main/java/com/upserve/uppend/blobs/NativeIO.java
@@ -1,52 +1,61 @@
 package com.upserve.uppend.blobs;
 
+import jnr.ffi.*;
+import jnr.ffi.types.size_t;
 import org.slf4j.Logger;
+import com.kenai.jffi.MemoryIO;
 
-import java.io.*;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.net.URL;
-import java.nio.MappedByteBuffer;
-import java.nio.file.Files;
+import java.nio.*;
 
 public class NativeIO {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private static final NativeC nativeC = LibraryLoader.create(NativeC.class).load("c");
+    static final int pageSize = nativeC.getpagesize(); // 4096 on most Linux
+
     public enum Advice {
-        Normal(0), Sequential(1), Random(2), WillNeed(3), DontNeed(4);
+        // These seem to be fairly stable https://github.com/torvalds/linux
+        // TODO add to https://github.com/jnr/jnr-constants
+        Normal(0), Random(1), Sequential(2), WillNeed(3), DontNeed(4);
         private final int value;
         Advice(int val) {
             this.value = val;
         }
     }
 
-    public static native void madvise(MappedByteBuffer buffer, Advice advise) throws IOException;
+    public interface NativeC {
+        int madvise(@size_t long address, @size_t long size, int advice);
+        int getpagesize();
+    }
 
-    static {
-        log.info("loading nativeIO libbrary...");
+    static long alignedAddress(long address) {
+        return address & (- pageSize);
+    }
 
-        try{
-            System.loadLibrary("nativeIO");
-            log.info("System.loadLibrary(\"nativeIO\") - successful!");
-        } catch (UnsatisfiedLinkError e) {
+    static long alignedSize(long address, int capacity) {
+        long end = address + capacity;
+        end = (end + pageSize - 1) & (-pageSize);
+        return end - alignedAddress(address);
+    }
 
-            String libName = "libnativeIO.dylib"; // The name of the file in resources/ dir
-            URL url = NativeIO.class.getResource("/" + libName);
-            File tmpDir = null;
-            try {
-                tmpDir = Files.createTempDirectory("nativeIO-lib").toFile();
-            } catch (IOException ex) {
-                throw new UncheckedIOException("Could not create tmpdir for nativeIO library", ex);
-            }
-            tmpDir.deleteOnExit();
-            File nativeLibTmpFile = new File(tmpDir, libName);
-            nativeLibTmpFile.deleteOnExit();
-            try (InputStream in = url.openStream()) {
-                Files.copy(in, nativeLibTmpFile.toPath());
-            } catch (IOException ex) {
-                throw new UncheckedIOException("Could not copy libnativeIO", ex);
-            }
-            System.load(nativeLibTmpFile.getAbsolutePath());
-            log.info("System.load(nativeLibTmpFile.getAbsolutePath()); - successful!");
+    public static void madvise(MappedByteBuffer buffer, Advice advice) throws IOException {
+
+        final long address = MemoryIO.getInstance().getDirectBufferAddress(buffer);
+        final int capacity = buffer.capacity();
+
+        long alignedAddress = alignedAddress(address);
+        long alignedSize = alignedSize(alignedAddress, capacity);
+
+        log.debug(
+                "Page size {}; Address: raw - {}, aligned - {}; Size: raw - {}, aligned - {}",
+                pageSize, address, alignedAddress, capacity, alignedSize
+        );
+        int val = nativeC.madvise(alignedAddress, alignedSize, advice.value);
+
+        if (val != 0) {
+            throw new IOException(String.format("System call madvise failed with code: %d", val));
         }
     }
 }
